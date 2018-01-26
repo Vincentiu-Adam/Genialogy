213d931e81d5e35919d820d420244f6c57357650
Vincent <vincentiu.adam@gmail.com>
2018-01-26 23:29:28 +0100

    Initial commit
    Added base gene and color gene

---
+++ Assets/Sources/Genes/BaseGene.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public enum GeneType
+{
+    HEAD,
+    BODY,
+    ARMS,
+    COLOR
+}
+
+[System.Serializable]
+public class BaseGene 
+{
+    [SerializeField]
+    private GeneType m_Type;
+}
